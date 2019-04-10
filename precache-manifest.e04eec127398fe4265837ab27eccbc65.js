2884dba1ea52407096ef0133da4fa0aa274ae8e4
Fabien Petitjean <f.petitjean@lombardodier.com>
2019-04-10 15:27:35 +0200

    Updates

---
+++ precache-manifest.e04eec127398fe4265837ab27eccbc65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "644320217d36375b7fe08d1e8f9d1c70",
+    "url": "/tfw-react-components/static/media/words-list.64432021.txt"
+  },
+  {
+    "revision": "7270440b8ddb42ebfe9fba9297a3563d",
+    "url": "/tfw-react-components/static/media/josefin.7270440b.woff2"
+  },
+  {
+    "revision": "915570368a08a08560b0",
+    "url": "/tfw-react-components/static/js/runtime~main.bf73a621.js"
+  },
+  {
+    "revision": "13188758a89047122b8d",
+    "url": "/tfw-react-components/static/js/main.2cdd3dd2.chunk.js"
+  },
+  {
+    "revision": "d2252444de841d12065b",
+    "url": "/tfw-react-components/static/js/2.23a66857.chunk.js"
+  },
+  {
+    "revision": "13188758a89047122b8d",
+    "url": "/tfw-react-components/static/css/main.6bf1453c.chunk.css"
+  },
+  {
+    "revision": "69b2cfa5dac44f839db66cfb0ab9a88d",
+    "url": "/tfw-react-components/index.html"
+  }
+];
